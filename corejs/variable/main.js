dba375850d956f078053c75b8b0aa4778e1dbaf4
sakhnaahan <saikhnaahan@gmail.com>
2025-09-22 17:04:10 +0800

    variable/array

---
+++ corejs/variable/main.js
@@ -0,0 +1,16 @@
+// const x = { a: 123 };
+// x.a = 456;
+// console.log(x);
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1000);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1000);
+// }
+
+// const arr = [1, 2];
+// const newArr = [...arr, 3]; // spread operator
+// console.log(newArr); // [1, 2, 3]
+// console.log(arr); // [1, 2] (original unchanged)
