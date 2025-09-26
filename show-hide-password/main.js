3ea3d11209921bad2bee08f76993d0e3436a7db8
sakhnaahan <saikhnaahan>
2025-09-26 20:44:45 +0800

    show&hide pasword

--- show-hide-password/main.js
+++ show-hide-password/main.js
@@ -1,1 +1,12 @@
-// test
+let input = document.querySelector("#passess");
+let showHide = document.querySelector("#show-hide");
+
+showHide.addEventListener("click", function () {
+  if (input.type === "password") {
+    input.type = "text";
+    showHide.textContent = "Hide";
+  } else {
+    input.type = "password";
+    showHide.textContent = "Show";
+  }
+});
