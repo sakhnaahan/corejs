41956a3bc9c791c59be711cb0421027d072ff82e
sakhnaahan <saikhnaahan@gmail.com>
2025-10-02 11:42:20 +0800

    palindrome checker fix

--- palindrome-checker/main.js
+++ palindrome-checker/main.js
@@ -1,22 +1,21 @@
-document.getElementById("check").addEventListener("click", function () {
-  const input = document.getElementById("input").value.trim();
-  const result = document.getElementById("result");
+const check = document.querySelector("#check");
+check.addEventListener("click", function () {
+  const input = document.querySelector("#input").value.trim();
+  const result = document.querySelector("#result");
 
   if (input) {
-    // normalize the text
-    const normal = input.replace(/[^-zA-Z0-9]/g, "").toLowerCase();
+    const normal = input.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
     const reverse = normal.split("").reverse().join("");
 
-    // check if it's a palindrome
     if (normal === reverse) {
-      result.textContent = `"${input}" is a palindrome!`;
+      result.textContent = `"${input}" is a palindrome`;
       result.style.color = "green";
     } else {
-      result.textContent = `"${input}" is not a palindrome.`;
+      result.textContent = `"${input}" is not a palindrome`;
       result.style.color = "red";
     }
   } else {
-    result.textContent = "Please enter some text.";
+    result.textContent = "Please add a text...";
     result.style.color = "orange";
   }
 });
